68cd4c3cef17d8f096ed4d61cc38f484602eb21e
Maycon Jardim <mayconsmill@gmail.com>
2022-05-27 20:38:28 -0300

    Criação do DTO da classe tecnico

--- src/main/java/com/helpdesk/api/resources/TecnicoResource.java
+++ src/main/java/com/helpdesk/api/resources/TecnicoResource.java
@@ -7,23 +7,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.helpdesk.api.dtos.TecnicoDTO;
 import com.helpdesk.api.entities.Tecnico;
 import com.helpdesk.api.services.TecnicoService;
 
 @RestController
 @RequestMapping(value = "/tecnicos")
 public class TecnicoResource {
-	
+
 	@Autowired
 	private TecnicoService service;
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
-		
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
+
 		Tecnico obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
-		
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
+
 	}
-	
-	
+
 }
