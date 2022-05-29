26f95b4f918cfe4d9d0a7879c0e2d0a65f6bddde
Maycon Jardim <mayconsmill@gmail.com>
2022-05-29 10:16:03 -0300

    endpoint create chamado

--- src/main/java/com/helpdesk/api/resources/ChamadoResource.java
+++ src/main/java/com/helpdesk/api/resources/ChamadoResource.java
@@ -1,11 +1,20 @@
 package com.helpdesk.api.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.helpdesk.api.dtos.ChamadoDTO;
 import com.helpdesk.api.entities.Chamado;
@@ -24,4 +33,19 @@ public class ChamadoResource {
 		return ResponseEntity.ok().body(new ChamadoDTO(obj));
 	}
 
+	@GetMapping
+	public ResponseEntity<Page<ChamadoDTO>> findAll(Pageable pageable){ 
+		Page<ChamadoDTO> list = service.findAllPaged(pageable);
+		return ResponseEntity.ok().body(list); 
+	}
+	
+	@PostMapping
+	public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO chamadoDTO) {
+		Chamado obj = service.create(chamadoDTO);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
 }
--- src/main/java/com/helpdesk/api/services/ChamadoService.java
+++ src/main/java/com/helpdesk/api/services/ChamadoService.java
@@ -2,23 +2,65 @@ package com.helpdesk.api.services;
 
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.helpdesk.api.dtos.ChamadoDTO;
 import com.helpdesk.api.entities.Chamado;
+import com.helpdesk.api.entities.Cliente;
+import com.helpdesk.api.entities.Tecnico;
+import com.helpdesk.api.enums.Prioridade;
+import com.helpdesk.api.enums.Status;
 import com.helpdesk.api.repositories.ChamadoRepository;
 import com.helpdesk.api.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ChamadoService {
 
-	
 	@Autowired
 	private ChamadoRepository repository;
-	
+
+	@Autowired
+	private TecnicoService tecnicoService;
+
+	@Autowired
+	private ClienteService clienteService;
+
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado ID: " + id));
 	}
-	
+
+	public Page<ChamadoDTO> findAllPaged(Pageable pageable) {
+		Page<Chamado> list = repository.findAll(pageable);
+		return list.map(x -> new ChamadoDTO(x));
+
+	}
+
+	public Chamado create(@Valid ChamadoDTO chamadoDTO) {
+		return repository.save(newChamado(chamadoDTO));
+	}
+
+	public Chamado newChamado(ChamadoDTO chamadoDTO) {
+		Tecnico tecnico = tecnicoService.findById(chamadoDTO.getTecnico());
+		Cliente cliente = clienteService.findById(chamadoDTO.getCliente());
+
+		Chamado chamado = new Chamado();
+		if (chamadoDTO.getId() != null) {
+			chamado.setId(chamadoDTO.getId());
+		}
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(chamadoDTO.getPrioridade()));
+		chamado.setStatus(Status.toEnum(chamadoDTO.getStatus()));
+		chamado.setTitulo(chamadoDTO.getTitulo());
+		chamado.setObservacoes(chamadoDTO.getObservacoes());
+		return chamado;
+
+	}
+
 }
