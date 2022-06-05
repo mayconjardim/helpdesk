1b70dffdd151e0c9aebd5b0586627b7fca033c32
Maycon Jardim <mayconsmill@gmail.com>
2022-06-05 11:54:48 -0300

    fix cliente senha

--- src/main/java/com/helpdesk/api/services/ClienteService.java
+++ src/main/java/com/helpdesk/api/services/ClienteService.java
@@ -54,6 +54,10 @@ public class ClienteService {
 	public Cliente update(@Valid Integer id, ClienteDTO clienteDTO) {
 		clienteDTO.setId(id);
 		Cliente obj = findById(id);
+		if(!clienteDTO.getSenha().equals(obj.getSenha())) {
+			clienteDTO.setSenha(encoder.encode(clienteDTO.getSenha()));
+		}
+		
 		validCpfEmail(clienteDTO);
 		obj = new Cliente(clienteDTO);
 		return repository.save(obj);
